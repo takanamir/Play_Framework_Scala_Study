87cad8793f6c43c08147f5a8b9988e94da8c18d7
Ryo Takanami <rtakanami0706gmail.com>
2020-06-22 12:19:28 +0900

    Hello World

---
+++ target/scala-2.13/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,15 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:C:/Users/TAKANAMI/play_trial/play-samples-scala/conf/routes
+// @DATE:Mon Jun 22 12:06:19 JST 2020
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
